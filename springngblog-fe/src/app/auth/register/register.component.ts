8f5e7d5a499f6a6adcdd028e0f7c9e46650919b9
myawsomworld <myawsomworld@outlook.com>
2023-05-15 16:41:54 -0500

    Angular dev
    Added Register component

--- springngblog-fe/src/app/auth/register/register.component.ts
+++ springngblog-fe/src/app/auth/register/register.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+registerForm: FormGroup;
+registerPayload: RegisterPayload;
 
+constructor(private formBuilder:FormBuilder){
+  this.formBuilder.group(controlConfig{
+    username:'',
+    email:'',
+    password:''.
+    confirmpassword:''
+  });
+
+  this.registerPayload={
+    username:'',
+    email:'',
+    password: '',
+    confirmpassword:''
+  };
+}
+
+
+ngOnInit(){}
+
+onSubmit(){
+  this.registerForm.get('username').value;
+  this.registerForm.get('email').value;
+  this.registerForm.get('password').value;
+  this.registerForm.get('confirmpassword').value;
+}
 }
