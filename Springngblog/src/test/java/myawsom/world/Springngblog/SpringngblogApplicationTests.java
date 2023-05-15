70b59af5e634de914ccbcc33ee6d2eb0177f76bc
myawsomworld <myawsomworld@outlook.com>
2023-05-15 10:16:50 -0500

    Springboot backend
    APIs for blog site

---
+++ Springngblog/src/test/java/myawsom/world/Springngblog/SpringngblogApplicationTests.java
@@ -0,0 +1,13 @@
+package myawsom.world.Springngblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringngblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
